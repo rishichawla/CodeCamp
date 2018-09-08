3d895c81dd5b8ae086c09c43b00b4d89970008b2
Rishi Chawla <rishi.chawla14@gmail.com>
2018-09-09 02:19:54 +0530

    Integrated psql

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, session
 from flask_session import Session
 
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -11,17 +12,29 @@ app.config["Session_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine('postgresql://postgres:admin@localhost/salesanalysis')
 db = scoped_session(sessionmaker(bind = engine))
 
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/details', methods = ["GET", "POST"])
+@app.route('/feed', methods = ["POST"])
 def details():
-    months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
-    return render_template("Details.html", months = months)
+    product_name = request.form.get("product_name")
+    price = request.form.get("price")
+    db.execute("INSERT into product (product_name, price) VALUES (:product_name, :price)", {"product_name": product_name, "price": price})
+    db.commit()
+    print(f"Succesfully added {{product_name}} that costs {{price}}")
+    details = db.execute("SELECT id from product where product_name = :product_name",{"product_name": product_name}).fetchone();
+    details_id = int(details.id)
+    month = request.form.get("month")
+    quantity = request.form.get("quantity")
+    db.execute("INSERT into details (month, quantity, details_id) VALUES (:month, :quantity, :details_id)", {"month": month, "quantity": quantity, "details_id": details_id})
+    db.commit()
+    print(f"Succesfully updated {{month}} that costs {{quantity}}")
+    return render_template("index.html")
+
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host='127.0.0.1', port=8080)
