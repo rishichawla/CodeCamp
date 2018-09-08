11ae16a26f6d1bac17a4a760db1bf2c7d98137fa
Rishi Chawla <rishi.chawla14@gmail.com>
2018-09-08 16:41:07 +0530

    Added database

--- application.py
+++ application.py
@@ -2,26 +2,26 @@ import os
 from flask import Flask, render_template, request, session
 from flask_session import Session
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 app = Flask(__name__)
 
 app.config["Session_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-data = {}
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind = engine))
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
-    name = request.form.get("item_name")
-    price = request.form.get("profit")
-    quantity = request.form.get("quantity")
-    month = request.form.get("month")
-    data[name] = [(price, quantity, month)]
     return render_template("index.html")
 
 @app.route('/details', methods = ["GET", "POST"])
 def details():
-    return render_template("Details.html", data = data)
+    months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+    return render_template("Details.html", months = months)
 if __name__ == '__main__':
     app.debug = True
     app.run()
