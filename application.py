7b1675384dd4e50450e08fb637a85d12abb50e3d
Rishi Chawla <rishi.chawla14@gmail.com>
2018-09-09 14:27:44 +0530

    Added Graphs and CSS

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask, render_template, request, session
+import base64
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, session, send_file
 from flask_session import Session
-
+from io import BytesIO
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -34,6 +37,38 @@ def details():
     db.commit()
     print(f"Succesfully updated {{month}} that costs {{quantity}}")
     return render_template("index.html")
+
+@app.route('/image')
+def piechart():
+    names = []
+    profit = []
+    plt.figure()
+    rows = db.execute("SELECT product_name, quantity, price from product JOIN details on details.details_id =  product.id").fetchall();
+    for row in rows:
+        names.append(row.product_name)
+        profit.append(row.quantity * row.price)
+    fig1, ax1 = plt.subplots()
+    ax1.pie(profit, labels = names, startangle = 90, autopct = '%1.1f%%')
+    ax1.axis('equal')
+    img = BytesIO()
+    plt.savefig(img, format = 'png')
+    img.seek(0)
+    return send_file(img, mimetype = 'image/png')
+
+@app.route('/image1')
+def bargraph():
+    names = []
+    profit = []
+    plt.figure()
+    rows = db.execute("SELECT product_name, quantity from product JOIN details on details.details_id =  product.id").fetchall();
+    for row in rows:
+        names.append(row.product_name)
+        profit.append(row.quantity)
+    plt.barh(names, profit)
+    img1 = BytesIO()
+    plt.savefig(img1, format = 'png')
+    img1.seek(0)
+    return send_file(img1, mimetype = 'image/png')
 
 if __name__ == '__main__':
     app.debug = True
